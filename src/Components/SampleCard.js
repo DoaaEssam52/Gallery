423a8e93cff5884a3eafd793ec9b81168cceaabf
Doaa Essam <due_essam_2015@yahoo.com>
2022-04-19 06:57:44 +0200

    Create carousel and initialize responsive

--- src/Components/SampleCard.js
+++ src/Components/SampleCard.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function SampleCard({ data , cardClicked}) {
   return (
-    <div className="sample-card" onClick={() => cardClicked(data)}>
-      <img src={data.url} />
+    <div className="sample-card" onClick={() => cardClicked(parseInt(data.index))}>
+      <div className="img-container" style={{backgroundImage:`url("${data.url}")`}}>
+      </div>
       <div className="sample-card__hover">
         <i className="fa-solid fa-magnifying-glass-plus"></i>
       </div>
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from "react";
 import "../Styles/App.css";
 import "../Styles/Loader.css";
 import "../Styles/SampleCard.css";
+import "../Styles/MediaQuery.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 /**********
  COMPONENTS 
@@ -16,6 +17,30 @@ import SampleCard from "./../Components/SampleCard";
 function App() {
   const [imagesData, setImagesData] = useState([]);
   const [isLoaded, setLoading] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    console.log("changed index=", currentIndex);
+  }, [currentIndex]);
+  const cardClicked = (data) => {
+    setCurrentIndex(data);
+  };
+  const previousImage = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(imagesData.length - 1);
+    }
+  };
+  const nextImage = () => {
+    if (currentIndex < imagesData.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+  const closeCarousel = () => {
+    setCurrentIndex(null);
+  };
   useEffect(() => {
     setTimeout(() => {
       fetch(
@@ -46,12 +71,42 @@ function App() {
               return (
                 <div key={item.uuid} className="app__main-section__img">
                   <SampleCard
+                    cardClicked={(data) => cardClicked(data)}
                     data={{ ...item, index }}
                   />
                 </div>
               );
             })}
           </div>
+          {currentIndex !== null && (
+            <>
+              <div className="app-carousel">
+                <i
+                  className="fa-solid fa-angles-left"
+                  onClick={() => previousImage()}
+                ></i>
+                <div className="app-carousel__img-container">
+                  <div className="app-carousel__img" style={{backgroundImage:`url("${imagesData[currentIndex].url}")`}}>
+
+                  </div>
+                  <p>{imagesData[currentIndex].name}</p>
+                </div>
+                <i
+                  className="fa-solid fa-angles-right"
+                  onClick={() => nextImage()}
+                ></i>
+              </div>
+              <div className="app-carousel-close">
+                <i
+                  className="fa-solid fa-rectangle-xmark"
+                  onClick={() => closeCarousel()}
+                ></i>
+              </div>
+              <div className="app-carousel__counter">
+                image {currentIndex+1} / {imagesData.length}
+              </div>
+            </>
+          )}
         </>
       ) : (
         <Loader />
